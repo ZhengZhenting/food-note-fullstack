9117900558ee9c90c75c3b87dd09bd905d5e4d32
ZhengZhenting <zhzh9685@bht-berlin.de>
2026-03-17 15:15:41 +0100

    update content page

--- backend-api/src/main/java/cn/kmbeast/service/impl/ContentNetServiceImpl.java
+++ backend-api/src/main/java/cn/kmbeast/service/impl/ContentNetServiceImpl.java
@@ -2,19 +2,26 @@ package cn.kmbeast.service.impl;
 
 import cn.kmbeast.context.LocalThreadHolder;
 import cn.kmbeast.mapper.ContentNetMapper;
+import cn.kmbeast.mapper.GourmetMapper;
 import cn.kmbeast.pojo.api.ApiResult;
 import cn.kmbeast.pojo.api.Result;
 import cn.kmbeast.pojo.dto.query.extend.ContentNetQueryDto;
+import cn.kmbeast.pojo.dto.query.extend.GourmetQueryDto;
 import cn.kmbeast.pojo.em.AuditEnum;
 import cn.kmbeast.pojo.entity.ContentNet;
 import cn.kmbeast.pojo.vo.ContentNetVO;
+import cn.kmbeast.pojo.vo.GourmetVO;
 import cn.kmbeast.service.ContentNetService;
 import jakarta.annotation.Resource;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -25,6 +32,8 @@ public class ContentNetServiceImpl implements ContentNetService {
 
     @Resource
     private ContentNetMapper contentNetMapper;
+    @Resource
+    private GourmetMapper gourmetMapper;
 
     /**
      * save
@@ -75,9 +84,62 @@ public class ContentNetServiceImpl implements ContentNetService {
      */
     @Override
     public Result<List<ContentNetVO>> query(ContentNetQueryDto contentNetQueryDto) {
-        List<ContentNetVO> contentNetList = contentNetMapper.query(contentNetQueryDto);
+        List<ContentNetVO> contentNetVOS = contentNetMapper.query(contentNetQueryDto);
         Integer totalCount = contentNetMapper.queryCount(contentNetQueryDto);
-        return ApiResult.success(contentNetList, totalCount);
+        return ApiResult.success(contentNetVOS, totalCount);
     }
 
+    /**
+     * get data through access password
+     *
+     * @param contentNetQueryDto new entity
+     * @return Result<Object> getting result
+     */
+    @Override
+    public Result<Object> findContent(ContentNetQueryDto contentNetQueryDto) {
+        List<ContentNetVO> contentNetVOS = contentNetMapper.query(contentNetQueryDto);
+        if(CollectionUtils.isEmpty(contentNetVOS)){
+            return ApiResult.success("Error");
+        }
+        ContentNetVO contentNetVO = contentNetVOS.get(0);
+
+        //valid days
+        LocalDateTime createTime = contentNetVO.getCreateTime();
+        LocalDateTime now = LocalDateTime.now();
+        long timeInbetween = ChronoUnit.DAYS.between(now, createTime);
+        if(contentNetVO.getValidDay() != -1 && timeInbetween>contentNetVO.getValidDay()){
+            return ApiResult.success("Link expired");
+        }
+
+        GourmetQueryDto gourmetQueryDto = new GourmetQueryDto();
+        gourmetQueryDto.setId(contentNetVO.getGourmetId());
+        List<GourmetVO> gourmetVOS = gourmetMapper.query(gourmetQueryDto);
+
+        //authentification
+        if(contentNetVO.getPasswordAuth() == null || !contentNetVO.getPasswordAuth()){
+            return ApiResult.success(gourmetVOS);
+        }
+
+        //valid password
+        if(!Objects.equals(contentNetVO.getAccessPassword(),contentNetQueryDto.getAccessPassword())){
+            return ApiResult.success("wrong access password");
+        }
+
+        return ApiResult.success(gourmetVOS);
+    }
+
+    /**
+     * if authentification necessary
+     *
+     * @param contentNetQueryDto new entity
+     * @return Result<Boolean> getting result
+     */
+    @Override
+    public Result<Boolean> authStatus(ContentNetQueryDto contentNetQueryDto) {
+        List<ContentNetVO> contentNetVOS = contentNetMapper.query(contentNetQueryDto);
+        if(CollectionUtils.isEmpty(contentNetVOS)){
+            return ApiResult.success("Error");
+        }
+        return ApiResult.success(contentNetVOS.get(0).getPasswordAuth());
+    }
 }
